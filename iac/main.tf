ed496633a0da0081fc540ae65f0a73f721ac7e0f
Claudio Toledo <claudiotoledoit@gmail.com>
2022-08-07 19:06:05 -0300

    Kube News version 2

--- iac/main.tf
+++ iac/main.tf
@@ -38,9 +38,6 @@ resource "digitalocean_kubernetes_node_pool" "kube01_nodepool" {
 
 resource "local_file" "kube_config" {
     content  = digitalocean_kubernetes_cluster.kube01.kube_config.0.raw_config
-    filename = "$HOME/kube_config.yaml"
+    filename = var.kube_config_path
 }
 
-variable "tkn" {}
-variable "k8s_name" {}
-variable "region" {}
---
+++ iac/variables.tf
@@ -0,0 +1,4 @@
+variable "tkn" {}
+variable "k8s_name" {}
+variable "region" {}
+variable "kube_config_path" {}
